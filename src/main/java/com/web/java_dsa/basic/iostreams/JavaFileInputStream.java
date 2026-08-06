446d1824147ba366c83ce2cca9f25b5be7debf66
Izzatbek <abdusharipovizzat03@gmail.com>
2026-08-06 15:41:38 +0500

    File'lar bilan CDRUD va I/O Streams dagi FileInputStream,FileOutputStream,BufferReader,BufferWriter class'lar o'rganildi

---
+++ src/main/java/com/web/java_dsa/basic/iostreams/JavaFileInputStream.java
@@ -0,0 +1,32 @@
+package com.web.java_dsa.basic.iostreams;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class JavaFileInputStream {
+    /*public static void main(String[] args) {
+        try (FileInputStream input = new FileInputStream("text.txt")) {
+            int i;
+            while ((i=input.read()) !=-1){
+                System.out.print((char) i);
+            }
+        } catch (IOException e){
+            System.out.print(e.getMessage());
+            e.printStackTrace();
+        }
+    }*/
+    public static void main(String[] args) {
+        try (FileInputStream input = new FileInputStream("text.txt")) {
+            FileOutputStream output = new FileOutputStream("code.txt");
+            int i;
+            while ((i=input.read())!=-1){
+                output.write(i);
+            }
+            System.out.println("File copied successfully");
+        } catch (IOException e){
+            System.out.print(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
