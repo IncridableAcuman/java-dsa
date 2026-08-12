35b8de896b76c815867faa4fb35f63a610e15365
Izzatbek <abdusharipovizzat03@gmail.com>
2026-08-12 13:47:22 +0500

    searching algorithms and hashSet problems solved

---
+++ src/main/java/com/web/java_dsa/basic/dsa/linked_list/LinkedListReverse.java
@@ -0,0 +1,22 @@
+package com.web.java_dsa.basic.dsa.linked_list;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class LinkedListReverse {
+    public static void main(String[] args) {
+        // LinkedList ni reverse qiling.
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i=0;i<n;i++){
+            int k = scanner.nextInt();
+            list.add(k);
+        }
+        Collections.reverse(list);
+        for (int x : list){
+            System.out.print(x + " ");
+        }
+    }
+}
